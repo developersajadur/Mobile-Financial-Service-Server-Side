5c209bf8bbd3e81e5a9f93b355740b9705e52a7c
developersajadur <itzmesojib@gmail.com>
2025-02-26 04:00:19 +0600

    install basic packages like, reduxt, tailwind, shadcn

---
+++ src/redux/features/auth/authSlice.ts
@@ -0,0 +1,46 @@
+import { RootState } from "@/redux/store";
+import { createSlice } from "@reduxjs/toolkit";
+
+export type TUser = {
+    userId: string;
+    fullName: string;
+    phoneNumber: string,
+    email: string;
+    role: string;
+    iat: number;
+    exp: number;
+  };
+
+type TAuthState = {
+    user: null | TUser;
+    token: null | string;
+  };
+
+const initialState: TAuthState = {
+    user: null,
+    token: null,
+  };
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setUser: (state, action) => {
+            const {user, token} = action.payload;
+            state.user = user;
+            state.token = token;
+        },
+        logOutUser: (state) => {
+            state.user = null;
+            state.token = null;
+        }
+    }
+})
+
+
+
+export const { setUser, logOutUser } = authSlice.actions;
+
+export default authSlice.reducer;
+export const useCurrentUser = (state: RootState) => state.auth._persist;
+export const useCurrentToken = (state: RootState) => state.auth._persist;
